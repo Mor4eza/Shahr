7ac0b30d38d42e36fc5fc9e69fb055396260de61
hamed <h.safarzadeh2014@gmail.com>
2015-06-15 05:41:56 -0700

    send and get

--- app/src/main/java/com/ariana/shahre_ma/WebServiceGet/HTTPGetInterestJson.java
+++ app/src/main/java/com/ariana/shahre_ma/WebServiceGet/HTTPGetInterestJson.java
@@ -1,9 +1,153 @@
 package com.ariana.shahre_ma.WebServiceGet;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.ariana.shahre_ma.DateBaseSqlite.DataBaseSqlite;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+
 /**
  * Created by ariana on 6/15/2015.
  */
-public class HTTPGetInterestJson
+public class HTTPGetInterestJson  extends AsyncTask<String, String, String>
 {
+    ProgressDialog pd;
 
+    private static Context context;
+    public HTTPGetInterestJson(Context c) {
+        context = c;
+    }
+    private static final String url_collection="http://test.shahrma.com/api/apigivecollection";
+
+    Integer Id[];
+    Integer subsetid[];
+    Integer memberid[];
+    Integer len;
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd = new ProgressDialog(context);
+        pd.setMessage("loading");
+        pd.show();
+    }
+
+    protected String doInBackground(String... args) {
+        try {
+
+
+            InputStream jsonStream = getStreamFromURL(url_collection, "GET");
+            String jsonString = streamToString(jsonStream);
+            parseJSON(jsonString);
+            onPostExecute();
+        } catch (Exception e) {
+            // Toast.makeText(getApplicationContext(),"do in background", Toast.LENGTH_LONG).show();
+        }
+        return null;
+
+
+    }
+
+    protected void onPostExecute() {
+        try {
+
+            DataBaseSqlite dbs = new DataBaseSqlite(context);
+
+
+            for (int i = 0; i <len; i++)
+            {
+                dbs.Add_Interest(Id[i], subsetid[i], memberid[i]);
+
+            }
+            pd.dismiss();
+        } catch (Exception e) {
+            Toast.makeText(context, "در پایگاه داده ذخیره نشد", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    void parseJSON(String JSONString) {
+
+        Integer ii = 0;
+        try {
+
+            JSONArray areas = new JSONArray(JSONString);
+
+            Id=new Integer[areas.length()];
+            subsetid=new Integer[areas.length()];
+            memberid=new Integer[areas.length()];
+            len=areas.length();
+            for (int i = 0; i < areas.length(); i++) {
+
+                JSONObject area = areas.getJSONObject(i);
+                Id[i]=area.getInt("Id");
+                subsetid[i]=area.getInt("Id");
+                memberid[i]=area.getInt("Id");
+
+
+            }
+
+        } catch (JSONException e) {
+            // Toast.makeText(getApplicationContext()," parse Json", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    InputStream getStreamFromURL(String urlString, String method) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            huc.setReadTimeout(10000);
+            huc.setConnectTimeout(15000);
+            huc.setRequestMethod(method);
+            huc.setDoInput(true);
+
+            huc.connect();
+
+            return huc.getInputStream();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    String streamToString(InputStream is) {
+        String result = "";
+        String line = null;
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                result = line + "\n";
+            }
+        } catch (Exception e) {
+        }
+
+
+        return result;
+    }
 }
+
+
+
