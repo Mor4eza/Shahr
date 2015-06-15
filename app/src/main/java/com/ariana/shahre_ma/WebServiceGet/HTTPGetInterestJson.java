5229e7bc41a74457980a35d2ceb85a8bb8b508fb
hamed <h.safarzadeh2014@gmail.com>
2015-06-15 05:18:09 -0700

    table like and interest

---
+++ app/src/main/java/com/ariana/shahre_ma/WebServiceGet/HTTPGetInterestJson.java
@@ -0,0 +1,9 @@
+package com.ariana.shahre_ma.WebServiceGet;
+
+/**
+ * Created by ariana on 6/15/2015.
+ */
+public class HTTPGetInterestJson
+{
+
+}
---
+++ app/src/main/java/com/ariana/shahre_ma/WebServiceGet/HTTPSendInterestURL.java
@@ -0,0 +1,7 @@
+package com.ariana.shahre_ma.WebServiceGet;
+
+/**
+ * Created by ariana on 6/15/2015.
+ */
+public class HTTPSendInterestURL {
+}
