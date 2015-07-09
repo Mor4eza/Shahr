c03aa1bd100e8085f1e85c251a251654834f4287
ariana2 <zigoool2013@gmail.com>
2015-07-09 11:39:17 +0430

    1394/4/18

--- app/src/main/java/com/ariana/shahre_ma/Cards/Comment_Card_Adapter.java
+++ app/src/main/java/com/ariana/shahre_ma/Cards/Comment_Card_Adapter.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
